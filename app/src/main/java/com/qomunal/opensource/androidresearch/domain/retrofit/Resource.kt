d771a6fd906d7e71650e8dce171282b6bfa74bda
amirisback <faisalamircs@gmail.com>
2025-02-18 14:03:45 +0700

    update: prepare for apollo client

--- app/src/main/java/com/qomunal/opensource/androidresearch/domain/retrofit/Resource.kt
+++ app/src/main/java/com/qomunal/opensource/androidresearch/domain/retrofit/Resource.kt
@@ -7,15 +7,3 @@ sealed class Resource<T>(val data: T? = null, val message: String? = null) {
     class Loading<T>(data: T? = null) : Resource<T>(data)
     class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
 }
-
-sealed class DownloadResource {
-    @Keep
-    data class Success(val localPath: String) : DownloadResource()
-
-    @Keep
-    data class Loading(val progress: Int? = 0) : DownloadResource()
-
-    @Keep
-    data class Error(val message: String) : DownloadResource()
-}
-
